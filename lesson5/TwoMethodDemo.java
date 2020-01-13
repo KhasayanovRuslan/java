72e78081ce96107b7a7857711e01dda9c98118de
KhasayanovRuslan <KhasayanovRuslan@yandex.ru>
2020-01-13 14:08:18 +0500

    многопоточность - урок 5

--- lesson5/TwoMethodDemo.java
+++ lesson5/TwoMethodDemo.java
@@ -4,15 +4,16 @@
     float[] arr = new float[size];
     float[] a1 = new float[h];
     float[] a2 = new float[h];
+    Thread t;
 
     class MyRunnableClass1 implements Runnable {
         @Override
         public void run() {
             try {
-                long t = System.currentTimeMillis();
-                for (int i = 0; i < h; i++) 
+                long time = System.currentTimeMillis();
+                for (int i = 0; i < h; i++)
                     a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                System.out.println(System.currentTimeMillis() - t);
+                System.out.println(System.currentTimeMillis() - time);
 
                 Thread.sleep(1000);
             }
@@ -21,15 +22,15 @@
             }
         }
     }
-        
+
     class MyRunnableClass2 implements Runnable {
         @Override
         public void run() {
             try {
-                long t = System.currentTimeMillis();
-                for (int i = 0; i < h; i++) 
+                long time = System.currentTimeMillis();
+                for (int i = 0; i < h; i++)
                     a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                System.out.println(System.currentTimeMillis() - t);
+                System.out.println(System.currentTimeMillis() - time);
 
                 Thread.sleep(1000);
             }
@@ -43,10 +44,10 @@
         for (int i = 0; i < size; i++) arr[i] = i;
 
         try {
-            long t = System.currentTimeMillis();
-            for (int i = 0; i < size; i++) 
+            long time = System.currentTimeMillis();
+            for (int i = 0; i < size; i++)
                 arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            System.out.println(System.currentTimeMillis() - t);
+            System.out.println(System.currentTimeMillis() - time);
 
             Thread.sleep(1000);
         }
@@ -54,45 +55,49 @@
             System.out.println("Прервано");
         }
 
-        System.out.println(arr);    
+        System.out.println(arr);
     }
 
     public synchronized void method2() {
         for (int i = 0; i < size; i++) arr[i] = i;
-         
-        long t = System.currentTimeMillis();
+
+        long time = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
-        System.out.println(System.currentTimeMillis() - t);
+        System.out.println(System.currentTimeMillis() - time);
 
         new Thread (new MyRunnableClass1()).start();
         new Thread (new MyRunnableClass2()).start();
-       
-        t = System.currentTimeMillis();
+
+        time = System.currentTimeMillis();
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
-        System.out.println(System.currentTimeMillis() - t);
+        System.out.println(System.currentTimeMillis() - time);
 
         System.out.println(arr);
     }
 
     TwoMethodDemo() {
-        Thread t = new Thread(this);
+        t = new Thread(this);
         t.start();
-    } 
+    }
 
     public void run() {
-        method1();
         method2();
     }
 
     public static void main(String[] args) {
-        new TwoMethodDemo();
+        TwoMethodDemo tmd = new TwoMethodDemo();
+        tmd.method1();
+
+        try {
+            tmd.t.join();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Прервано");
+        }
     }
-}
-
-
-     
+}     
 
 
          
